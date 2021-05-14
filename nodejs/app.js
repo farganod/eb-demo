6dd24aee723c47cbda025a324745f43f610eb660
Fargano <fargano@amazon.com>
2021-05-14 10:35:28 -0500

    check

--- nodejs/app.js
+++ nodejs/app.js
@@ -18,8 +18,15 @@ const try_connect_sql = async (hostname, username, pass) => {
     password : pass
     //database: db
   });
-  connection.connect();
-  return true;
+  var check = connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return false;
+    }
+    console.log('connected as id ' + connection.threadId);
+    return true;
+  });
+  return check;
 };
 
 var server = http.createServer(async (req, res) => {
